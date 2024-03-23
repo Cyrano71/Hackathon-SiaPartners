9ad94929aef74978744e721518afa9f631c2286b
Cyrano75 <berthierjg@gmail.com>
2024-03-23 21:41:36 +0100

    clean js

--- scripts/interact_legasi.js
+++ scripts/interact_legasi.js
@@ -2,16 +2,17 @@ const { ethers } = require("hardhat");
 
 async function callMyContract() {
   const MyContract = await ethers.getContractFactory("LegasiV0");
-  const contract = MyContract.attach(
-    // The deployed contract address
-    "0xfEA1961cD4cd613782d8d2d324d3d17a61Ddb4AB"
-  );
 
   const deadManAddress = "0x6CFb9257ef8E80edD6a268c51D28d2D5D597688a"
   const notaryAddress = "0xE6a5086276722F8D0BBAf259190Fd7d62F0F05a8"
   const heirAddress = "0x69c8c68623e3bE3F4C351F67586eD13DF3E0Cac8"
   const erc20 = "0x794c3C2BaF6fCA02EfbF69350885DBB2a0Dd2e93";
-  
+  const legaciAddress = "0xfEA1961cD4cd613782d8d2d324d3d17a61Ddb4AB"
+
+  const contract = MyContract.attach(
+    legaciAddress
+  );
+
   /*
   //By dead man
   console.log(await contract.addBalance(erc20, 10,{ gasLimit: 1500000 }))
@@ -28,10 +29,8 @@ async function callMyContract() {
   console.log(await contract.getDispatch(deadManAddress))
   */
 
-  /*
-  By owner
-  console.log(await contract.transferFunds(deadManAddress))
-  */
+  //By owner
+  //console.log(await contract.transferFunds(deadManAddress))
   
 }
 
--- scripts/interact_mytoken.js
+++ scripts/interact_mytoken.js
@@ -7,10 +7,16 @@ async function callMyContract() {
     "0x794c3C2BaF6fCA02EfbF69350885DBB2a0Dd2e93"
   );
 
+  /*
   const legasiAddress = "0xfEA1961cD4cd613782d8d2d324d3d17a61Ddb4AB"
   console.log(await contract.balanceOf(legasiAddress))
   console.log(await contract.transfer(legasiAddress, 100000))
   console.log(await contract.balanceOf(legasiAddress))
+  */
+ /*
+  const heirAddress = "0x69c8c68623e3bE3F4C351F67586eD13DF3E0Cac8"
+  console.log(await contract.balanceOf(heirAddress))
+  */
 }
 
 callMyContract().catch((error) => {
