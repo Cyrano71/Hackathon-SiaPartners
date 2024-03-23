3ac1824622ba8c05f7f98da48059847b780d08d9
Cyrano75 <berthierjg@gmail.com>
2024-03-23 18:19:16 +0100

    approve notary

--- contracts/LegasiV0.sol
+++ contracts/LegasiV0.sol
@@ -4,12 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LegasiV0 {
 
-    struct Token {
-        address erc20;
-        uint256 amount;
-    }
-
-    mapping (address => Token[]) userTokens;
+    mapping (address => address[]) userTokens;
 
     mapping (address => bool) notaries;
 
@@ -37,14 +32,27 @@ contract LegasiV0 {
         return notaries[notary];
     }
 
-    function addToken(address token, uint256 amount) external {
-        Token[] storage tokens = userTokens[msg.sender];
-        tokens.push(Token(token, amount));
+    function transferToNotary(address token, address notary) external {
+        address[] storage tokens = userTokens[msg.sender];
+        tokens.push(token);
+        IERC20(token).approve(notary, 10000 * 1e18);
     }
 
-    function balanceOf(address from) external view notaryOnly returns (Token[] memory) {
-        Token[] memory tokens = userTokens[from];
-        return tokens;
+    struct Balance {
+        address erc20;
+        uint256 value;
+    }
+
+    function balanceOf(address from) external view notaryOnly returns (Balance[] memory) {
+        address[] memory tokens = userTokens[from];
+        uint256 length = tokens.length;
+        Balance[] memory balances = new Balance[](length);
+        for (uint256 i = 0; i < length; i++) 
+        {   
+            address token = tokens[i];
+            balances[i] = Balance(token, IERC20(token).balanceOf(from));
+        }
+        return balances;
     }
 
     struct Transfer {
@@ -53,14 +61,14 @@ contract LegasiV0 {
     }
 
     function transferFunds(address from, Transfer[] calldata to) external notaryOnly {
-        Token[] memory tokens = userTokens[from];
+        address[] memory tokens = userTokens[from];
         uint256 length = tokens.length;
         uint256 nbHeirs = to.length;
         for (uint256 i = 0; i < length; i++) 
         {   
             for (uint256 j = 0; j < nbHeirs; j++) 
             {
-                IERC20(tokens[i].erc20).transfer(to[i].heir, tokens[i].amount);
+                IERC20(tokens[i]).transfer(to[i].heir, to[i].amount);
             }  
         }
     }
